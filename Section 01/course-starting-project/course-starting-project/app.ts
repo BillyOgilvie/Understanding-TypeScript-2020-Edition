d7cff991772b191eed481b505612669b583e8be7
BillyOgilvie <43520983+BillyOgilvie@users.noreply.github.com>
2022-03-21 20:41:49 +0000

    up to  The 'any' Type

--- Section 01/course-starting-project/course-starting-project/app.ts
+++ Section 01/course-starting-project/course-starting-project/app.ts
@@ -1,1 +1,12 @@
-console.log('Your code goes here...');
+function add(n1: number, n2: number, showResult: boolean, phrase = ''): number {
+  const result = n1 + n2;
+  if (showResult) console.log(phrase + result);
+  else return result;
+}
+
+const number1 = 5;
+const number2 = 2.8;
+const printResult = true;
+const resultPharse = 'Result is: ';
+
+add(number1, number2, printResult, resultPharse);
--- Section 02/basics-05-object-types/app.ts
+++ Section 02/basics-05-object-types/app.ts
@@ -1,10 +1,45 @@
+// const person: { name: string; age: number } = {
 // const person: {
 //   name: string;
 //   age: number;
+//   hobbies: string[];
+//   role: [number, string];
 // } = {
+//   name: 'Bill',
+//   age: 30,
+//   hobbies: ['Sports', 'Cooking'],
+//   role: [2, 'author'],
+// };
+
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+} // behind the scences, this translates to {0, 1, 2}
+
 const person = {
-  name: 'Maximilian',
-  age: 30
+  name: 'Bill',
+  age: 30,
+  hobbies: ['Sports', 'Cooking'],
+  role: Role.ADMIN,
 };
 
+// person.role.push('admin'); // this is an exception. It should error but works
+// person.role[1] = 10; // Type 'number' is not assignable to type 'string'
+
+// person.role = [0, 'admin', 'user']; // Type '[number, string, string]' is not assignable to type '[number, string]'. Source has 3 element(s) but target allows only 2
+
+console.log(person.role);
+
+let favoriteActivites: string[];
+favoriteActivites = ['Sports'];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
+
+if (person.role === Role.ADMIN) {
+  console.log('user is read only');
+}
