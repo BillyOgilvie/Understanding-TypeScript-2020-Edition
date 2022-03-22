8cfe0d3c1c43aa90113d35b7952985fe34df3f8f
bogilvie <billy.ogilvie@anaplan.com>
2022-03-22 09:41:21 +0000

    section 2 done

--- Section 02/basics-05-object-types/app.ts
+++ Section 02/basics-05-object-types/app.ts
@@ -1,45 +1,18 @@
-// const person: { name: string; age: number } = {
-// const person: {
-//   name: string;
-//   age: number;
-//   hobbies: string[];
-//   role: [number, string];
-// } = {
-//   name: 'Bill',
-//   age: 30,
-//   hobbies: ['Sports', 'Cooking'],
-//   role: [2, 'author'],
-// };
+let userInput: unknown;
+let userName: string;
 
-enum Role {
-  ADMIN,
-  READ_ONLY,
-  AUTHOR,
-} // behind the scences, this translates to {0, 1, 2}
+userInput = 5;
+userInput = 'Max';
+// userName = userInput; // Type 'unknown' is not assignable to type 'string'
 
-const person = {
-  name: 'Bill',
-  age: 30,
-  hobbies: ['Sports', 'Cooking'],
-  role: Role.ADMIN,
-};
-
-// person.role.push('admin'); // this is an exception. It should error but works
-// person.role[1] = 10; // Type 'number' is not assignable to type 'string'
-
-// person.role = [0, 'admin', 'user']; // Type '[number, string, string]' is not assignable to type '[number, string]'. Source has 3 element(s) but target allows only 2
-
-console.log(person.role);
-
-let favoriteActivites: string[];
-favoriteActivites = ['Sports'];
-
-console.log(person.name);
-
-for (const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase());
+if (typeof userInput === 'string') {
+  userName = userInput;
 }
 
-if (person.role === Role.ADMIN) {
-  console.log('user is read only');
+// throwing an error always 'crashes' the script/block, so will NEVER return. This is a different type to VOID
+// ie. you are telling other devs this function is intended to NEVER return. e.g. while (true) {xyz...} will be type never too
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
 }
+
+generateError('An error occured', 500);
