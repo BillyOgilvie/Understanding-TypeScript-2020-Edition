9586f028053dbedd2dc0d588c90ad3891ac04578
BillyOgilvie <43520983+BillyOgilvie@users.noreply.github.com>
2022-03-28 18:28:06 +0100

    Another Generic Function

--- Section 07/generics-01-starting-setup/src/app.ts
+++ Section 07/generics-01-starting-setup/src/app.ts
@@ -10,3 +10,31 @@
 // promise.then((data) => {
 //   data.toLowerCase();
 // });
+
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+console.log(merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 }));
+
+const mergeObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
+console.log(mergeObj);
+
+interface Lengty {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengty>(element: T): [T, string] {
+  let descriptionText = 'Got no value.';
+  if (element.length === 1) {
+    descriptionText = `Got 1 element`;
+  }
+  if (element.length > 1) {
+    descriptionText = `Got ${element.length} elements`;
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe('Hi there!'));
+console.log(countAndDescribe(['Sports', 'Cooking']));
+console.log(countAndDescribe(''));
