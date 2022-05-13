eb086cbea6a1ae1abc2bc595f4d3f601866e3927
BillyOgilvie <43520983+BillyOgilvie@users.noreply.github.com>
2022-05-13 16:24:36 +0100

    Understanding various Import & Export Syntaxes

--- Section 09/prj-00-initial-starting-setup/src/components/project-item.ts
+++ Section 09/prj-00-initial-starting-setup/src/components/project-item.ts
@@ -1,4 +1,4 @@
-import { Component } from './base-components.js';
+import Component from './base-components.js';
 import { Project } from '../models/project.js';
 import { autobind } from '../decorators/autobind.js';
 import { Draggable } from '../models/drag-drop.js';
