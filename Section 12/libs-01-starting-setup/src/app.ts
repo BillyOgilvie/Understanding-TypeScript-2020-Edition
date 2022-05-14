7bf45e10329bac2d012af28f2fabf4aed6e397d5
BillyOgilvie <43520983+BillyOgilvie@users.noreply.github.com>
2022-05-14 15:29:17 +0100

    No Types Needed: class-transformer

--- Section 12/libs-01-starting-setup/src/app.ts
+++ Section 12/libs-01-starting-setup/src/app.ts
@@ -1,5 +1,19 @@
-import * as _ from 'lodash';
+import 'reflect-metadata';
+import { plainToInstance } from 'class-transformer';
+import { Product } from './product.model';
 
-console.log(_.shuffle([1, 2, 3]));
+const products = [
+  { title: 'A Carpet', price: 29.99 },
+  { title: 'A Book', price: 10.99 },
+];
+//const p1 = new Product('A book', 12.99);
 
-console.log('Hi');
+// const loadedProducts = products.map(
+//   (prod) => new Product(prod.title, prod.price)
+// );
+
+const loadedProducts = plainToInstance(Product, products);
+
+for (const prod of loadedProducts) {
+  console.log(prod.getInformation());
+}
