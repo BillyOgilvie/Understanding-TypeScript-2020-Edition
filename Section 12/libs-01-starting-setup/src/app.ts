947c28b88b3a382ed7dff1fb9fbfd303ffd92fac
BillyOgilvie <43520983+BillyOgilvie@users.noreply.github.com>
2022-05-14 15:37:27 +0100

    TypeScript-embracing: class-validator

--- Section 12/libs-01-starting-setup/src/app.ts
+++ Section 12/libs-01-starting-setup/src/app.ts
@@ -1,12 +1,24 @@
 import 'reflect-metadata';
 import { plainToInstance } from 'class-transformer';
 import { Product } from './product.model';
+import { validate } from 'class-validator';
 
 const products = [
   { title: 'A Carpet', price: 29.99 },
   { title: 'A Book', price: 10.99 },
 ];
-//const p1 = new Product('A book', 12.99);
+
+const prod = new Product('', -12.99);
+validate(prod).then((errors) => {
+  if (errors.length > 0) {
+    console.log('Validation Errors');
+    console.log(errors);
+  } else {
+    console.log(prod.getInformation());
+  }
+});
+
+// const p1 = new Product('A book', 12.99);
 
 // const loadedProducts = products.map(
 //   (prod) => new Product(prod.title, prod.price)
