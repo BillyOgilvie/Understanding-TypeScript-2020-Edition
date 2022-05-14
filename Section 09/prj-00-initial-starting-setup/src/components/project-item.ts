8cc451d2b9d3f4fde602ed000447243a8a7cd15c
BillyOgilvie <43520983+BillyOgilvie@users.noreply.github.com>
2022-05-14 10:59:37 +0100

    Adding Entry & Output Configuration

--- Section 09/prj-00-initial-starting-setup/src/components/project-item.ts
+++ Section 09/prj-00-initial-starting-setup/src/components/project-item.ts
@@ -1,7 +1,7 @@
-import Component from './base-components.js';
-import { Project } from '../models/project.js';
-import { autobind } from '../decorators/autobind.js';
-import { Draggable } from '../models/drag-drop.js';
+import Component from './base-components';
+import { Project } from '../models/project';
+import { autobind } from '../decorators/autobind';
+import { Draggable } from '../models/drag-drop';
 
 export class ProjectItem
   extends Component<HTMLUListElement, HTMLLIElement>
